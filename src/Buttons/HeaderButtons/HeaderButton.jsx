d5c283f182c15ba940fb882d4138d83ad903a225
Omar Ibanez <omar.ibanez343@gmail.com>
2024-05-22 21:48:21 -0600

    New project, wii

---
+++ src/Buttons/HeaderButtons/HeaderButton.jsx
@@ -0,0 +1,7 @@
+function HeaderButton() {
+    return (
+        <button className="bg-white text-black">Click me</button>
+    )
+}
+
+export default HeaderButton;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import HeaderButton from './Buttons/HeaderButtons/HeaderButton.jsx'
+import MainTemplate from './Buttons/MainTemplate/MainTemplate.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    {/* <App /> */}
+    {/* <HeaderButton /> */}
+    <MainTemplate />
+  </>,
+)
